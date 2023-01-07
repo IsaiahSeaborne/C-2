e22581649c704c4d787dab75ae62328dd5ed839b
IsaiahSeaborne <77293329+IsaiahSeaborne@users.noreply.github.com>
2023-01-07 15:33:43 -0500

    Add files via upload

---
+++ Collection/StateCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    #region State
+    public class StateCollection
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Capitol { get; set; }
+        public int Population { get; set; }
+        public static List<StateCollection> StateList
+        {
+            get
+            {
+                var list = new List<StateCollection>();
+                foreach (var s in states)
+                {
+                    list.Add(s);
+                }
+                list.AddRange(states);
+                return list;
+            }
+        }
+        public static SortedDictionary<string, string> StateSD
+        {
+            get
+            {
+                var StatesSD = new SortedDictionary<string, string>();
+                foreach (var s in states)
+                {
+                    StatesSD.Add(s.Code,s.Name);
+                }
+                return StatesSD;
+
+            }
+        }
+        public static SortedList<string, string> StateSL
+        {
+            get
+            {
+                var StateSL = new SortedList<string, string>();
+                foreach (var s in states)
+                {
+                    StateSL.Add(s.Code, s.Name);
+                }
+                return StateSL;
+
+            }
+        }
+        private static StateCollection[] states =
+        {
+            new StateCollection("Virginia", "VA", "Richmond", 1234234),
+            new StateCollection("Maryland", "MY", "UrM", 2341234),
+            new StateCollection("Florida", "FL", "Idk", 923423),
+            new StateCollection("New York", "NY", "sadf", 323445),
+            new StateCollection("Texas", "TX", "Houston", 538834)
+        };
+        public StateCollection(string name, string code, string capitol, int population)
+        {
+            Name = name;
+            Code = code;
+            Capitol = capitol;
+            Population = population;
+        }
+        //properties
+        ///constructor
+        //and property to create like students
+    }
+    #endregion
+}
---
+++ Collection/StudentCode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    #region Student
+    public class Student
+    //sorted dictionary help with homework. sort by primary key
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public float GPA { get; set; }
+        public static List<Student> Students
+        {
+            get
+            {
+                var list = new List<Student>();
+                //--< Add code here >--//
+                foreach(var s in students)
+                {
+                    list.Add(s);
+                }
+                list.AddRange(students);
+                return list;
+            }
+        }
+        public static Dictionary<int, Student> StudentDictionary
+        {
+            get
+            {
+                var dictionary = new Dictionary<int, Student>();
+                //--< Add code here >--//
+                foreach (var s in students)
+                {
+                    dictionary.Add(s.StudentId, s);
+                }
+                return dictionary;
+
+            }
+        }
+        public static Stack<Student> StudentStack
+        {
+            get
+            {
+                var stack = new Stack<Student>();
+                //--< Add code here >--//
+                foreach(var s in students)
+                {
+                    stack.Push(s);
+                }
+                return stack;
+            }
+        }
+        public static Queue<Student> StudentQueue
+        {
+            get
+            {
+                var queue = new Queue<Student>();
+                //--< Add code here >--//
+                foreach(var s in students)
+                {
+                    queue.Enqueue(s);
+                }
+                return queue;
+            }
+        }
+        private static Student[] students =
+        {
+            new Student(101, "Skip Wythe", 3.43f),
+            new Student(108, "James River", 2.89f),
+            new Student(107, "Maggie Walker", 3.49f),
+            new Student(105, "Tre deGar", 2.77f),
+            new Student(109, "Chip N. Ham", 3.08f)
+        };
+        private static void Playground()
+        {
+            var student = students[0];
+            student = students[4];
+        }
+        public Student(int studentId, string name, float gpa)
+        {
+            StudentId = studentId;
+            Name = name;
+            GPA = gpa;
+        }
+    }
+    #endregion
+}
