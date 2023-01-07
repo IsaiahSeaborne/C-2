d1e53a6f687646a5e2103a2c10ec771cf89d00ef
IsaiahSeaborne <77293329+IsaiahSeaborne@users.noreply.github.com>
2023-01-07 15:33:06 -0500

    Add files via upload

---
+++ TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("<----- Student: Isaiah Seaborne ----->");
+
+            //Debugging.Debugging.Test();
+            //Collection.TestStudentCollection.TestStudentCollections();
+            //Collection.TestStateCollection.TestStateCollections();
+            //Collection.EmployeePay.EmployeeTest.Test();
+
+            //EDI.TestEDI.Test();
+            //Sales.SalesTest.Test();
+            Purchasing.PurchasingTest.Test();
+            ReadKey();
+        }
+    }
+}
