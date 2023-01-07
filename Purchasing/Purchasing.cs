c866b5ceeaf46e1627924e7496374e9b4e11e3db
IsaiahSeaborne <77293329+IsaiahSeaborne@users.noreply.github.com>
2023-01-07 15:23:23 -0500

    Add files via upload

---
+++ Purchasing/Purchasing.cs
@@ -0,0 +1,164 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Purchasing
+{
+    class Purchasing
+    {
+
+        static PurchasingEntities db = new PurchasingEntities();
+
+        public static List<VendorPurchase> VendorPurchases
+        {
+            get
+            {
+                //--< Replace the following line with your code >--//
+                return new List<VendorPurchase>();                    
+            }
+        }
+    }
+    public class VendorPurchase
+    {
+        public int VendorId { get; set; }
+        public string Vendor { get; set; }
+        public decimal TotalPurchases { get; set; }
+    }
+    public partial class Part
+    {
+
+        static PurchasingEntities1 db = new PurchasingEntities1();
+
+        public int OrderCount => db.PurchaseOrders.Count(po => po.Amount > 200);
+        //public decimal TotalPurchases => db.PurchaseOrders.Sum(po => po.Amount);
+        public static List<PartPurchase> PartPurchases
+        {
+            get
+            {
+                //--< Replace the following line with your code >--//
+                return new List<PartPurchase>();
+            }
+        }
+    }
+    public class PartPurchase
+    {
+        public int PartId { get; set; }
+        public string Part { get; set; }
+        public decimal TotalPurchases { get; set; }
+    }
+    public partial class PurchaseOrderPart
+    {
+        
+        public static string StudentName 
+        { 
+            get
+            {
+                var name = "Isaiah Seaborne";
+
+                return name;
+            }
+                
+        }
+        static PurchasingEntities1 db = new PurchasingEntities1();
+        public int QuantityReceived
+        {
+            get
+            {
+                var qr = (from pops in db.Receipts
+                          
+                          select pops.Quantity).Sum();
+                return qr;
+            }
+        }
+        public int BackOrdered 
+        { 
+            get
+            {
+                var bo =
+                    (from pops in db.PurchaseOrderParts
+                 select pops.Quantity).Sum();
+
+                return bo - QuantityReceived;
+            }
+        }
+        
+        public decimal CostReceived
+        {
+            get
+            {
+                return (from pops in db.Receipts
+                        select pops.TotalCost).Sum();
+            }
+        }
+    }
+    
+    public partial class PurchaseOrder
+    {
+        static PurchasingEntities1 db = new PurchasingEntities1();
+        public decimal? TotalAmount 
+        { 
+            get
+            {
+                var total = (from pops in db.PurchaseOrderParts
+                             select pops.Cost).Sum();
+                return total;
+            }
+        }
+        public static List<PurchaseOrder> OpenOrders 
+        {
+            get
+            {
+                var pos = (from pops in db.PurchaseOrders
+                           where pops.PurchaseOrderStatusID == POSIStatus.Open
+                           select pops).ToList();
+                return pos;
+            }
+        }
+        public static List<dynamic> MonthlySales 
+        { 
+            get
+            {
+                var date = (from pops in db.PurchaseOrders
+                            group pops by pops.PODate.Month into monthGroup
+                            select new
+                            {
+                                Month = monthGroup.Key,
+                                TotalAmount = monthGroup.Sum(po => po.TotalAmount)
+
+                            }).ToList<dynamic>();
+
+                return date;
+
+                
+            }
+        }
+    }
+    public partial class Vendor
+    {
+        static PurchasingEntities1 db = new PurchasingEntities1();
+        public decimal TotalAmount 
+        { 
+            get
+            {
+                var total = (from pops in db.PurchaseOrders
+                             select pops.Amount).Sum();
+                return (decimal)total;
+            } 
+        }
+        public static List<PurchaseOrder> Top3 
+        { 
+            get
+            {
+                var top3 = db.PurchaseOrders.OrderByDescending(x => x.Amount).Take(3).ToList();
+
+                return top3;
+            }
+        }
+    }
+}
---
+++ Purchasing/Test.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.Objects;
+
+namespace Purchasing
+{
+    public class PurchasingTest
+    {
+        public static void Test()
+        {
+            //TestUpdateVendor();
+            //TestView();
+            //TestFunction();
+            TestStoredProcedure();
+        }
+        private static void TestUpdateVendor()
+        {
+            //Update PurchaseOrder set PurchaseOrderStatusId = 1
+            using (var db = new PurchasingEntities())
+            {
+                //C in crud create
+                var vendor = db.Vendors.Find(28);
+                vendor = new Vendor()
+                {
+                    Name = "Mr.Bobbie",
+                };
+                db.Vendors.Add(vendor);
+                db.SaveChanges();
+
+                //U in crud update
+                var purchaseorder = db.PurchaseOrders.Find(2);
+                var purchaseorderstatus = purchaseorder.PurchaseOrderStatusID;
+                purchaseorder.PurchaseOrderStatusID = POSIStatus.Open;
+
+                
+                var name = vendor.Name;
+                vendor.Name = "Poopie";
+
+                db.SaveChanges();
+
+                
+
+                //D in Crud delete record
+                vendor = db.Vendors.Find(28);
+                db.Vendors.Remove(vendor);
+                db.SaveChanges();
+            }
+
+        }
+        private static void TestView()
+
+        {
+            using (var db = new PurchasingEntities1())
+            {
+                var summaries = db.PoSummaries;
+
+                foreach (var s in summaries)
+                {
+                    Console.WriteLine($"{s.PurchaseOrderId}\t{ s.PODate}\t{s.Vendor}\t{s.TotalAmount}");
+                }
+            }
+        
+        }
+        private static void TestFunction()
+        {
+            using (var db = new PurchasingEntities1())
+            {
+                var details = db.PoDetail(4);
+                foreach (var detail in details)
+                {
+                    Console.WriteLine($"{detail.PurchaseOrderId}" +
+                        $"\t{detail.PODate}" +
+                        $"\t{detail.Vendor}" +
+                        $"\t{detail.Part}" +
+                        $"\t{detail.Quantity}" +
+                        $"\t{detail.Value}");
+                }
+
+            }
+
+        }
+        private static void TestStoredProcedure()
+        {
+            int poNum = 1;
+            Console.WriteLine("Before Stored Procedure");
+
+            using (var db = new PurchasingEntities1())
+            {
+                var po = db.PurchaseOrders.Find(1);
+                po.Amount = 0;
+                db.SaveChanges();
+
+                Console.WriteLine($"{po.PurchaseOrderId}\t{po.Amount}");
+
+                ObjectParameter amount = new ObjectParameter("Amount", 0);
+
+                db.POP(poNum, amount);
+
+                Console.WriteLine("After SP");
+            }
+
+            using (var db = new PurchasingEntities1())
+            {
+                var po = db.PurchaseOrders.Find(1);
+                Console.WriteLine($"{po.PurchaseOrderId}\t{po.Amount}");
+            }
+
+        }
+
+    }
+}
