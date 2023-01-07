04def7cd916a46db8546b46aa8e3eed39e04f978
IsaiahSeaborne <77293329+IsaiahSeaborne@users.noreply.github.com>
2023-01-07 15:17:48 -0500

    Add files via upload

---
+++ EDI/Delegate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDI
+{
+    public class Delegate
+    {
+        delegate int DateDifference(DateTime startTime, DateTime endTime);
+
+        public static void DelegateTest()
+        {
+            DateTime startTime = new DateTime(2022, 10, 1);
+            DateTime endTime = new DateTime(2022, 10, 4);
+
+            //this is a method call, you need to set this to the Program.Days 
+            DateDifference code = Days;
+            int diffInDays = code(startTime, endTime);
+
+            code = Hours;
+            int diffInHours = code(startTime, endTime);
+
+            code = Minutes;
+            int diffInMinutes = code(startTime, endTime);
+
+        }
+
+        public static int Days(DateTime startTime, DateTime endTime)
+        {
+            int diffInDays = (endTime.Date - startTime.Date).Days;
+            return diffInDays;
+
+        }
+        public static int Hours(DateTime startTime, DateTime endTime)
+        {
+            int diffInHours = ((endTime.Date - startTime.Date).Days * 24);
+            return diffInHours;
+        }
+        public static int Minutes(DateTime startTime, DateTime endTime)
+        {
+            int diffInMinutes = (((endTime.Date - startTime.Date).Days * 24) * 60);
+            return diffInMinutes;
+        }
+
+
+
+        delegate int Numbers(int[] numbers);
+        public static void DelegateTutorial()
+        {
+            int[] numbers = { 42, 7, 14, 63, 21, 70, 49, 28, 35, 56 };
+            Numbers code = SumTheNumbers;
+            int total = code(numbers);
+
+
+            code = MaximumNumber;
+            int max = code(numbers);
+
+            code = delegate (int[] nums) { return nums[0]; };
+            int first = code(numbers);
+
+            Console.WriteLine($"The total is {total}");
+            Console.WriteLine($"The largest number is {max}");
+            Console.WriteLine($"The first number is {first}");
+        }
+
+        private static int MaximumNumber(int[] numbers)
+        {
+            var max = int.MinValue;
+            foreach (var num in numbers)
+                if (num > max)
+                    max = num;
+            return max;
+        }
+        private static int SumTheNumbers(int[] numbers)
+        {
+            int total = 0;
+            foreach (int num in numbers)
+                total += num;
+            return total;
+        }
+    }
+}
---
+++ EDI/Extension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDI
+{
+    public static class Extension
+    {
+        
+        public static string Left(this string name, int length)
+        {
+            if (name == null)
+                return null;
+            int stringLength = name.Length;
+            if (length >= stringLength)
+                return name;
+            return name.Substring(0, length);
+        }
+        public static string Right(this string name, int length)
+        {
+            if (name == null)
+                return null;
+            int stringLength = name.Length;
+            if (length >= stringLength)
+                return name;
+            return name.Substring(stringLength - length, length);
+        }
+        public static string ZipCode(this int zip)
+        {
+            
+
+            string zipp = zip.ToString().PadLeft(5, '0');
+            Console.WriteLine(zipp);
+            return zipp;
+            
+        }
+    }
+    public class ExtensionTutorial
+    {
+        public ExtensionTutorial()
+        {
+            var name = "Amanda Shelton";
+            var firstName = name.Left(3);
+            var lastName = name.Right(4);
+            Console.WriteLine($"Name: {name}\t\tFirstName: {firstName}");
+            Console.WriteLine($"LastName: {lastName}");
+        }
+    }
+   
+}
